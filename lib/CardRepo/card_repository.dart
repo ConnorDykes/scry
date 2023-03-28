8028d9e0e14022f644a85568397afa6f80a42927
Connor Dykes <connormdykes@gmail.com>
2023-03-27 21:43:18 -0600

    Card Search by name

--- lib/CardRepo/card_repository.dart
+++ lib/CardRepo/card_repository.dart
@@ -1,1 +1,57 @@
-class CardRepository {}
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:scry/card_model.dart';
+
+class CardRepository {
+  Future<List<CardModel>?> searchNamed({required String name}) async {
+    final client = http.Client();
+    final query = name.toLowerCase().replaceAll(" ", "+");
+    debugPrint(query);
+
+    var uri = Uri.parse('https://api.scryfall.com/cards/search?q=$query ');
+    try {
+      var response = await client.get(uri);
+      if (response.statusCode == 200) {
+        debugPrint('got Cards');
+        List<CardModel> cards = [];
+
+        getPage({required String url}) async {
+          debugPrint('getting more pages');
+          var uri = Uri.parse(url);
+          var response = await client.get(uri);
+          if (response.statusCode == 200) {
+            final body = json.decode(response.body);
+            List data = body['data'];
+            List<CardModel> page =
+                List<CardModel>.from(body.map((x) => CardModel.fromJson(x)));
+            cards.addAll(page);
+            if (body['has_more'] == true) {
+              debugPrint(cards.length.toString());
+              getPage(url: body['next_page']);
+            } else {
+              return cards;
+            }
+          }
+        }
+
+        final body = json.decode(response.body);
+        List data = body['data'];
+        List<CardModel> page =
+            List<CardModel>.from(data.map((x) => CardModel.fromJson(x)));
+        cards.addAll(page);
+        if (body['has_more'] == true) {
+          debugPrint('has more pages');
+          getPage(url: body['next_page']);
+        }
+        return cards;
+      }
+      if (response.statusCode == 404) {
+        debugPrint(response.body);
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:scry/CardRepo/card_repository.dart';
+import 'package:scry/card_model.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +17,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        textTheme: GoogleFonts.latoTextTheme(),
+        dividerColor: Colors.grey[400],
+        textTheme: GoogleFonts.rubikTextTheme(),
         colorScheme: ColorScheme(
             surface: Colors.white,
             onSurface: Colors.black,
@@ -45,30 +48,116 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<CardModel>? cards = [];
+  var loaded = false;
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  void initState() {
+    // getCards();
+    super.initState();
+  }
+
+  // getCards() async {
+  //   cards = await CardRepository().searchNamed();
+  //   if (cards != null) {
+  //     setState(() {
+  //       loaded = true;
+  //     });
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Text(
-          'Scry',
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          title: Text(
+            'Scry',
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-                textAlign: TextAlign.center,
-                'HomePage: This is example text that I will use to determin isf the font I have selected is acceptbla e in this app, IF not I will try to find a baeter font'),
-          ],
-        ),
-      ),
-      // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Visibility(
+          visible: loaded,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  onChanged: (value) async {
+                    var response =
+                        await CardRepository().searchNamed(name: value);
+                    if (response != null) {
+                      setState(() {
+                        loaded = true;
+                        cards = response;
+                      });
+                    }
+                  },
+                  controller: searchController,
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 8),
+                      fillColor: theme.cardColor,
+                      filled: true,
+                      hintText: "Search Card Name",
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: theme.primaryColor),
+                          borderRadius: BorderRadius.circular(15)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(15))),
+                ),
+              ),
+              Container(
+                color: Colors.grey[400],
+                constraints: BoxConstraints(maxHeight: 1),
+              ),
+              Visibility(
+                visible: loaded,
+                child: Expanded(
+                  child: ListView.builder(
+                    padding: EdgeInsets.zero,
+                    itemCount: cards!.length,
+                    itemBuilder: (context, index) {
+                      final card = cards![index];
+                      final imageSmall = card.imageUris?.small ?? '';
+                      final imageNormal = card.imageUris?.normal ?? '';
+
+                      return Card(
+                        child: ListTile(
+                          onTap: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return Dialog(
+                                    child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(14),
+                                        child: Image.network(imageNormal)),
+                                  );
+                                });
+                          },
+                          shape: RoundedRectangleBorder(),
+                          leading: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: imageSmall == ''
+                                ? Icon(Icons.photo)
+                                : Image.network(
+                                    imageSmall,
+                                  ),
+                          ),
+                          title: Text(card.name ?? ''),
+                          trailing: Icon(Icons.chevron_right_rounded),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
